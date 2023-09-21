3cf541ad73aa511083cf9ef8ab761b5492291449
vladi <vladislav_fedotov_official@bk.ru>
2023-09-21 18:32:49 +0300

    Initial commit

---
+++ src/main/java/com/petprojects/jwtsecuritywebapp/JwtSecurityWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.petprojects.jwtsecuritywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtSecurityWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/petprojects/jwtsecuritywebapp/JwtSecurityWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petprojects.jwtsecuritywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecurityWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
